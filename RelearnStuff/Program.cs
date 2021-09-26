32e5cd891dc892c4ef31705525f702f5c83eb350
ArikMackenburg <Arikmackenburg@gmail.com>
2021-09-26 13:16:31 -0500

    Relearning and what not

---
+++ RelearnStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RelearnStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
